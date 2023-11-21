4f33b7269cb5670b95a0118cff949a34e6062e88
Intrstng <intrstg@gmail.com>
2023-11-21 19:30:54 +0300

    feat: complete task HW4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,12 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
     // НУЖНО ДОПИСАТЬ
+    props.callBack();
   };
 
   return (
@@ -14,3 +15,26 @@ export const Button = (props: ButtonPropsType) => {
     </button>
   );
 };
+
+
+
+
+
+
+
+// type ButtonPropsType = {
+//   callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+//   name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+// };
+//
+// export const Button = (props: ButtonPropsType) => {
+//   const callBackHandler = () => {
+//     // НУЖНО ДОПИСАТЬ
+//   };
+//
+//   return (
+//       <button id={'hw04-button'} onClick={callBackHandler}>
+//         {props.name}
+//       </button>
+//   );
+// };
